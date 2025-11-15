1ef0cd43ea93594c7bc92203cdbda82382cb136b
Daniel Yoann <64357912+F4IGV@users.noreply.github.com>
2025-11-15 12:29:40 +0100

    Update script to run dapnet_metar_final.py

--- run_dapnet_metar_LFRN.bat
+++ run_dapnet_metar_LFRN.bat
@@ -1,4 +1,5 @@
 @echo off
 REM Lance le script Python pour envoyer la météo solaire sur DAPNET
 cd /d "C:\Scripts\DAPNET"
-py "dapnet_metar_LFRN.py" >> "C:\Scripts\DAPNET\dapnet_metar_LFRN.log" 2>&1
+
+py "dapnet_metar_final.py" >> "C:\Scripts\DAPNET\dapnet_metar_final.log" 2>&1
