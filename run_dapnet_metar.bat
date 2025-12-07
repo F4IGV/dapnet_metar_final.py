bec045f656079fe000986b82c4ce1ed5b397e0e0
Daniel Yoann <64357912+F4IGV@users.noreply.github.com>
2025-12-07 11:54:31 +0100

    Update run_dapnet_metar.bat with folder comment
    Added a comment to specify the folder containing the .py file.

--- run_dapnet_metar.bat
+++ run_dapnet_metar.bat
@@ -1,5 +1,6 @@
 @echo off
 REM Lance le script Python pour envoyer la météo solaire sur DAPNET
-cd /d "C:\Scripts\DAPNET"
+cd /d "C:\Scripts\DAPNET"  #choose your folder who contain the .py file
 
 py "dapnet_metar_final.py" >> "C:\Scripts\DAPNET\dapnet_metar_final.log" 2>&1
+
